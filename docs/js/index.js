0794f54c4f27e77e3a4423f8b4b17d2fcfd51a90
Peyton Gasink <peytongasink@att.net>
2020-12-27 17:42:48 -0600

    Finished the demo for placement customizations

--- docs/js/index.js
+++ docs/js/index.js
@@ -1,3 +1,15 @@
-document.getElementById("createToast").onclick = function () {
+// Getting Started
+var gettingStartedBtn = document.getElementById("gettingStartedBtn");
+gettingStartedBtn.onclick = function () {
     Toast.create("Voila!", "How easy was that?", TOAST_STATUS.SUCCESS, 5000);
-}
+};
+
+// Placement
+var placementBtns = document.querySelectorAll(".placement-grid-btn");
+placementBtns.forEach(function (btn) {
+    btn.onclick = function () {
+        var placement = btn.getAttribute("data-placement");
+        Toast.setPlacement(parseInt(placement));
+        Toast.create("Placement Updated", `Current placement: ${btn.innerText}`, TOAST_STATUS.SUCCESS, 5000);
+    };
+});
