ed37daf75f094e0dc0a1aef8c99a109c38db4938
Peyton Gasink <peyton@peytongasink.dev>
2021-09-12 20:11:38 -0500

    removed commented code

--- docs/js/index.js
+++ docs/js/index.js
@@ -1,7 +1,3 @@
-// import { Toast, TOAST_STATUS } from "../../js/bootstrap-toaster.js";
-
-// Toast.enableTimers(TOAST_TIMERS.COUNTDOWN);
-
 // Getting Started
 var timeoutInput = document.getElementById("timeoutInput");
 var timeout = 5000;
