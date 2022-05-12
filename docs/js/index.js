b027943c868f41b82d3de4959cd37e63e4210986
Peyton Gasink <peyton@peytongasink.dev>
2022-05-11 20:47:40 -0500

    now we're cooking with gas

--- docs/js/index.js
+++ docs/js/index.js
@@ -1,5 +1,5 @@
-// import Toast, { TOAST_STATUS } from "../../umd/bundle.js";
-import * as Toast from "../../js/index.esm.js";
+// import Toast, { BootstrapToaster.TOAST_STATUS } from "../../umd/bundle.js";
+import * as BootstrapToaster from "../../js/index.esm.js";
 
 // Getting Started
 var timeoutInput = document.getElementById("timeoutInput");
@@ -9,7 +9,7 @@ timeoutInput.onchange = () => {
     timeout = timeoutInput.value * 1000;
     let toast = {
         title: "Timeout Updated",
-        status: Toast.TOAST_STATUS.SUCCESS,
+        status: BootstrapToaster.TOAST_STATUS.SUCCESS,
         timeout: timeout
     }
     if (timeout !== 0)
@@ -17,8 +17,7 @@ timeoutInput.onchange = () => {
     else
         toast.message = "New toasts must be manually dismissed.";
 
-    Toast.create(toast);
-    // Toast.DANGER
+    BootstrapToaster.create(toast);
 };
 
 var successBtn = document.getElementById("successBtn");
@@ -26,10 +25,10 @@ successBtn.onclick = () => {
     let toast = {
         title: "Voila!",
         message: "How easy was that?",
-        status: Toast.TOAST_STATUS.SUCCESS,
+        status: BootstrapToaster.TOAST_STATUS.SUCCESS,
         timeout: timeout
     }
-    Toast.create(toast);
+    BootstrapToaster.create(toast);
 };
 
 var dangerBtn = document.getElementById("dangerBtn");
@@ -37,10 +36,10 @@ dangerBtn.onclick = () => {
     let toast = {
         title: "Oops",
         message: "That's no good.",
-        status: Toast.TOAST_STATUS.DANGER,
+        status: BootstrapToaster.TOAST_STATUS.DANGER,
         timeout: timeout
     }
-    Toast.create(toast);
+    BootstrapToaster.create(toast);
 };
 
 var warningBtn = document.getElementById("warningBtn");
@@ -48,10 +47,10 @@ warningBtn.onclick = () => {
     let toast = {
         title: "Heads Up",
         message: "You might want to look into that.",
-        status: TOAST_STATUS.WARNING,
+        status: BootstrapToaster.TOAST_STATUS.WARNING,
         timeout: timeout
     }
-    Toast.create(toast);
+    BootstrapToaster.create(toast);
 };
 
 var infoBtn = document.getElementById("infoBtn");
@@ -61,10 +60,10 @@ infoBtn.onclick = () => {
         message: "I am <a href=\"https://www.halopedia.org/2401_Penitent_Tangent\" class=\"link-danger\" target=\"_blank\"" +
             " rel=\"noopener\">2401 Penitent Tangent</a>. I am the Monitor of Installation 05. A Reclaimer? Here? At last! We have much to do." +
             " This facility must be activated if we are to control this outbreak.",
-        status: TOAST_STATUS.INFO,
+        status: BootstrapToaster.TOAST_STATUS.INFO,
         timeout: timeout
     }
-    Toast.create(toast);
+    BootstrapToaster.create(toast);
 };
 
 // Placement
@@ -72,14 +71,14 @@ var placementBtns = document.querySelectorAll(".placement-grid-btn");
 placementBtns.forEach(btn => {
     btn.onclick = () => {
         var placement = parseInt(btn.getAttribute("data-placement"));
-        Toast.setPlacement(placement);
+        BootstrapToaster.setPlacement(placement);
         let toast = {
             title: "Placement Updated",
             message: `Current placement: ${btn.innerText}`,
-            status: TOAST_STATUS.SUCCESS,
+            status: BootstrapToaster.TOAST_STATUS.SUCCESS,
             timeout: timeout
         }
-        Toast.create(toast);
+        BootstrapToaster.create(toast);
     };
 });
 
@@ -88,14 +87,14 @@ var maxCountInput = document.getElementById("maxCountInput");
 var maxCount = 4;
 maxCountInput.onchange = () => {
     maxCount = maxCountInput.value;
-    Toast.setMaxCount(maxCount);
+    BootstrapToaster.setMaxCount(maxCount);
     let toast = {
         title: "Max Count Updated",
         message: `Only ${maxCount} toasts can appear at once.`,
-        status: TOAST_STATUS.SUCCESS,
+        status: BootstrapToaster.TOAST_STATUS.SUCCESS,
         timeout: timeout
     }
-    Toast.create(toast);
+    BootstrapToaster.create(toast);
 };
 
 // Theming
@@ -103,14 +102,14 @@ var themeBtns = document.querySelectorAll(".theme-btn");
 themeBtns.forEach(btn => {
     btn.onclick = () => {
         var theme = parseInt(btn.getAttribute("data-theme"));
-        Toast.setTheme(theme);
+        BootstrapToaster.setTheme(theme);
         let toast = {
             title: "Theme Updated",
             message: `Current theme: ${btn.innerText}`,
-            status: TOAST_STATUS.SUCCESS,
+            status: BootstrapToaster.TOAST_STATUS.SUCCESS,
             timeout: timeout
         }
-        Toast.create(toast);
+        BootstrapToaster.create(toast);
     };
 });
 
@@ -119,14 +118,14 @@ var queueBtns = document.querySelectorAll(".queue-btn");
 queueBtns.forEach(btn => {
     btn.onclick = () => {
         var enableQueue = parseInt(btn.getAttribute("data-queue"));
-        Toast.enableQueue(enableQueue);
+        BootstrapToaster.enableQueue(enableQueue);
         let toast = {
             title: "Queue Setting Updated",
             message: `The toast queue has been ${enableQueue ? "enabled" : "disabled"}.`,
-            status: TOAST_STATUS.SUCCESS,
+            status: BootstrapToaster.TOAST_STATUS.SUCCESS,
             timeout: timeout
         }
-        Toast.create(toast);
+        BootstrapToaster.create(toast);
     };
 });
 
@@ -135,13 +134,13 @@ var timerBtns = document.querySelectorAll(".timer-btn");
 timerBtns.forEach(btn => {
     btn.onclick = () => {
         var useTimers = parseInt(btn.getAttribute("data-timer"));
-        Toast.enableTimers(useTimers);
+        BootstrapToaster.enableTimers(useTimers);
         let toast = {
             title: "Timer Setting Updated",
             message: `New toasts will have ${btn.innerText.toLowerCase()} timers.`,
-            status: TOAST_STATUS.SUCCESS,
+            status: BootstrapToaster.TOAST_STATUS.SUCCESS,
             timeout: timeout
         }
-        Toast.create(toast);
+        BootstrapToaster.create(toast);
     };
 });
