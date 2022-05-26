abdf3b9944f6fa8d27b8d1d3ed636d583df61f49
Peyton Gasink <peyton@peytongasink.dev>
2022-05-25 22:31:29 -0500

    umd and esm seem to both work now!

--- docs/js/index.js
+++ docs/js/index.js
@@ -4,142 +4,144 @@
 var timeoutInput = document.getElementById("timeoutInput");
 var timeout = 5000;
 timeoutInput.onchange = () => {
-    // convert seconds to milliseconds
-    timeout = timeoutInput.value * 1000;
-    let toast = {
-        title: "Timeout Updated",
-        status: BootstrapToaster.TOAST_STATUS.SUCCESS,
-        timeout: timeout
-    }
-    if (timeout !== 0)
-        toast.message = `Toasts will disappear after ${timeoutInput.value} seconds.`;
-    else
-        toast.message = "New toasts must be manually dismissed.";
+  // convert seconds to milliseconds
+  timeout = timeoutInput.value * 1000;
+  let toast = {
+    title: "Timeout Updated",
+    status: BootstrapToaster.TOAST_STATUS.SUCCESS,
+    timeout: timeout,
+  };
+  if (timeout !== 0)
+    toast.message = `Toasts will disappear after ${timeoutInput.value} seconds.`;
+  else toast.message = "New toasts must be manually dismissed.";
 
-    BootstrapToaster.create(toast);
+  BootstrapToaster.create(toast);
 };
 
 var successBtn = document.getElementById("successBtn");
 successBtn.onclick = () => {
-    let toast = {
-        title: "Voila!",
-        message: "How easy was that?",
-        status: BootstrapToaster.TOAST_STATUS.SUCCESS,
-        timeout: timeout
-    }
-    BootstrapToaster.create(toast);
+  let toast = {
+    title: "Voila!",
+    message: "How easy was that?",
+    status: BootstrapToaster.TOAST_STATUS.SUCCESS,
+    timeout: timeout,
+  };
+  BootstrapToaster.create(toast);
 };
 
 var dangerBtn = document.getElementById("dangerBtn");
 dangerBtn.onclick = () => {
-    let toast = {
-        title: "Oops",
-        message: "That's no good.",
-        status: BootstrapToaster.TOAST_STATUS.DANGER,
-        timeout: timeout
-    }
-    BootstrapToaster.create(toast);
+  let toast = {
+    title: "Oops",
+    message: "That's no good.",
+    status: BootstrapToaster.TOAST_STATUS.DANGER,
+    timeout: timeout,
+  };
+  BootstrapToaster.create(toast);
 };
 
 var warningBtn = document.getElementById("warningBtn");
 warningBtn.onclick = () => {
-    let toast = {
-        title: "Heads Up",
-        message: "You might want to look into that.",
-        status: BootstrapToaster.TOAST_STATUS.WARNING,
-        timeout: timeout
-    }
-    BootstrapToaster.create(toast);
+  let toast = {
+    title: "Heads Up",
+    message: "You might want to look into that.",
+    status: BootstrapToaster.TOAST_STATUS.WARNING,
+    timeout: timeout,
+  };
+  BootstrapToaster.create(toast);
 };
 
 var infoBtn = document.getElementById("infoBtn");
 infoBtn.onclick = () => {
-    let toast = {
-        title: "Greetings!",
-        message: "I am <a href=\"https://www.halopedia.org/2401_Penitent_Tangent\" class=\"link-danger\" target=\"_blank\"" +
-            " rel=\"noopener\">2401 Penitent Tangent</a>. I am the Monitor of Installation 05. A Reclaimer? Here? At last! We have much to do." +
-            " This facility must be activated if we are to control this outbreak.",
-        status: BootstrapToaster.TOAST_STATUS.INFO,
-        timeout: timeout
-    }
-    BootstrapToaster.create(toast);
+  let toast = {
+    title: "Greetings!",
+    message:
+      'I am <a href="https://www.halopedia.org/2401_Penitent_Tangent" class="link-danger" target="_blank"' +
+      ' rel="noopener">2401 Penitent Tangent</a>. I am the Monitor of Installation 05. A Reclaimer? Here? At last! We have much to do.' +
+      " This facility must be activated if we are to control this outbreak.",
+    status: BootstrapToaster.TOAST_STATUS.INFO,
+    timeout: timeout,
+  };
+  BootstrapToaster.create(toast);
 };
 
 // Placement
 var placementBtns = document.querySelectorAll(".placement-grid-btn");
-placementBtns.forEach(btn => {
-    btn.onclick = () => {
-        var placement = parseInt(btn.getAttribute("data-placement"));
-        BootstrapToaster.setPlacement(placement);
-        let toast = {
-            title: "Placement Updated",
-            message: `Current placement: ${btn.innerText}`,
-            status: BootstrapToaster.TOAST_STATUS.SUCCESS,
-            timeout: timeout
-        }
-        BootstrapToaster.create(toast);
+placementBtns.forEach((btn) => {
+  btn.onclick = () => {
+    var placement = parseInt(btn.getAttribute("data-placement"));
+    BootstrapToaster.setPlacement(placement);
+    let toast = {
+      title: "Placement Updated",
+      message: `Current placement: ${btn.innerText}`,
+      status: BootstrapToaster.TOAST_STATUS.SUCCESS,
+      timeout: timeout,
     };
+    BootstrapToaster.create(toast);
+  };
 });
 
 // Max Count
 var maxCountInput = document.getElementById("maxCountInput");
 var maxCount = 4;
 maxCountInput.onchange = () => {
-    maxCount = maxCountInput.value;
-    BootstrapToaster.setMaxCount(maxCount);
-    let toast = {
-        title: "Max Count Updated",
-        message: `Only ${maxCount} toasts can appear at once.`,
-        status: BootstrapToaster.TOAST_STATUS.SUCCESS,
-        timeout: timeout
-    }
-    BootstrapToaster.create(toast);
+  maxCount = maxCountInput.value;
+  BootstrapToaster.setMaxCount(maxCount);
+  let toast = {
+    title: "Max Count Updated",
+    message: `Only ${maxCount} toasts can appear at once.`,
+    status: BootstrapToaster.TOAST_STATUS.SUCCESS,
+    timeout: timeout,
+  };
+  BootstrapToaster.create(toast);
 };
 
 // Theming
 var themeBtns = document.querySelectorAll(".theme-btn");
-themeBtns.forEach(btn => {
-    btn.onclick = () => {
-        var theme = parseInt(btn.getAttribute("data-theme"));
-        BootstrapToaster.setTheme(theme);
-        let toast = {
-            title: "Theme Updated",
-            message: `Current theme: ${btn.innerText}`,
-            status: BootstrapToaster.TOAST_STATUS.SUCCESS,
-            timeout: timeout
-        }
-        BootstrapToaster.create(toast);
+themeBtns.forEach((btn) => {
+  btn.onclick = () => {
+    var theme = parseInt(btn.getAttribute("data-theme"));
+    BootstrapToaster.setTheme(theme);
+    let toast = {
+      title: "Theme Updated",
+      message: `Current theme: ${btn.innerText}`,
+      status: BootstrapToaster.TOAST_STATUS.SUCCESS,
+      timeout: timeout,
     };
+    BootstrapToaster.create(toast);
+  };
 });
 
 // Queue
 var queueBtns = document.querySelectorAll(".queue-btn");
-queueBtns.forEach(btn => {
-    btn.onclick = () => {
-        var enableQueue = parseInt(btn.getAttribute("data-queue"));
-        BootstrapToaster.enableQueue(enableQueue);
-        let toast = {
-            title: "Queue Setting Updated",
-            message: `The toast queue has been ${enableQueue ? "enabled" : "disabled"}.`,
-            status: BootstrapToaster.TOAST_STATUS.SUCCESS,
-            timeout: timeout
-        }
-        BootstrapToaster.create(toast);
+queueBtns.forEach((btn) => {
+  btn.onclick = () => {
+    var enableQueue = parseInt(btn.getAttribute("data-queue"));
+    BootstrapToaster.enableQueue(enableQueue);
+    let toast = {
+      title: "Queue Setting Updated",
+      message: `The toast queue has been ${
+        enableQueue ? "enabled" : "disabled"
+      }.`,
+      status: BootstrapToaster.TOAST_STATUS.SUCCESS,
+      timeout: timeout,
     };
+    BootstrapToaster.create(toast);
+  };
 });
 
 // Timers
 var timerBtns = document.querySelectorAll(".timer-btn");
-timerBtns.forEach(btn => {
-    btn.onclick = () => {
-        var useTimers = parseInt(btn.getAttribute("data-timer"));
-        BootstrapToaster.enableTimers(useTimers);
-        let toast = {
-            title: "Timer Setting Updated",
-            message: `New toasts will have ${btn.innerText.toLowerCase()} timers.`,
-            status: BootstrapToaster.TOAST_STATUS.SUCCESS,
-            timeout: timeout
-        }
-        BootstrapToaster.create(toast);
+timerBtns.forEach((btn) => {
+  btn.onclick = () => {
+    var useTimers = parseInt(btn.getAttribute("data-timer"));
+    BootstrapToaster.enableTimers(useTimers);
+    let toast = {
+      title: "Timer Setting Updated",
+      message: `New toasts will have ${btn.innerText.toLowerCase()} timers.`,
+      status: BootstrapToaster.TOAST_STATUS.SUCCESS,
+      timeout: timeout,
     };
+    BootstrapToaster.create(toast);
+  };
 });
--- src/js/bootstrap-toaster.ts
+++ src/js/bootstrap-toaster.ts
@@ -1,4 +1,4 @@
-// import { Toast } from "bootstrap"; appears to be broken at the moment, see https://github.com/twbs/bootstrap/issues/31944
+import Toast from "bootstrap/js/src/toast";
 
 /**
  * Copyright (c) 2022 Peyton Gasink
@@ -37,53 +37,53 @@ TOAST_TEMPLATE.innerHTML = `
 
 /** Defines the valid status options for toasts. */
 enum TOAST_STATUS {
-    SUCCESS = 1,
-    DANGER,
-    WARNING,
-    INFO
+  SUCCESS = 1,
+  DANGER,
+  WARNING,
+  INFO,
 }
 /** Defines the valid placement options for the toast container. */
 enum TOAST_PLACEMENT {
-    TOP_LEFT = 1,
-    TOP_CENTER,
-    TOP_RIGHT,
-    MIDDLE_LEFT,
-    MIDDLE_CENTER,
-    MIDDLE_RIGHT,
-    BOTTOM_LEFT,
-    BOTTOM_CENTER,
-    BOTTOM_RIGHT
+  TOP_LEFT = 1,
+  TOP_CENTER,
+  TOP_RIGHT,
+  MIDDLE_LEFT,
+  MIDDLE_CENTER,
+  MIDDLE_RIGHT,
+  BOTTOM_LEFT,
+  BOTTOM_CENTER,
+  BOTTOM_RIGHT,
 }
 /** Defines the valid options for toast themes. */
 enum TOAST_THEME {
-    LIGHT = 1,
-    DARK
+  LIGHT = 1,
+  DARK,
 }
 /** Defines the valid options for toast header timers. */
 enum TOAST_TIMERS {
-    DISABLED,
-    ELAPSED,
-    COUNTDOWN,
+  DISABLED,
+  ELAPSED,
+  COUNTDOWN,
 }
 
 interface IToast {
-    toast: HTMLElement;
-    timeout: number;
+  toast: HTMLElement;
+  timeout: number;
 }
 
 interface IToastOptions {
-    title: string,
-    message: string,
-    status?: TOAST_STATUS,
-    timeout?: number
+  title: string;
+  message: string;
+  status?: TOAST_STATUS;
+  timeout?: number;
 }
 
 interface IConfiguration {
-    maxToasts?: number;
-    placement?: TOAST_PLACEMENT;
-    theme?: TOAST_THEME;
-    enableTimers?: TOAST_TIMERS;
-    enableQueue?: boolean;
+  maxToasts?: number;
+  placement?: TOAST_PLACEMENT;
+  theme?: TOAST_THEME;
+  enableTimers?: TOAST_TIMERS;
+  enableQueue?: boolean;
 }
 
 /** Maximum amount of toasts to be allowed on the page at once. */
@@ -102,11 +102,11 @@ let queue: IToast[] = [];
  * @param {IConfiguration} options Object containing all the desired toast options.
  */
 function configure(options: IConfiguration): void {
-    setMaxCount(options?.maxToasts);
-    setPlacement(options?.placement);
-    setTheme(options?.theme);
-    enableTimers(options?.enableTimers);
-    enableQueue(options?.enableQueue)
+  setMaxCount(options?.maxToasts);
+  setPlacement(options?.placement);
+  setTheme(options?.theme);
+  enableTimers(options?.enableTimers);
+  enableQueue(options?.enableQueue);
 }
 
 /**
@@ -114,14 +114,15 @@ function configure(options: IConfiguration): void {
  * @param {number} maxToasts Maximum number of toasts allowed on the page at once.
  */
 function setMaxCount(maxToasts: number): void {
-    if (maxToasts !== null) {
-        if (maxToasts > 0) {
-            maxToastCount = maxToasts;
-        }
-        else {
-            console.error("The maximum number of toasts must be greater than 0. Reverting to default.");
-        }
+  if (maxToasts !== null) {
+    if (maxToasts > 0) {
+      maxToastCount = maxToasts;
+    } else {
+      console.error(
+        "The maximum number of toasts must be greater than 0. Reverting to default."
+      );
     }
+  }
 }
 
 /**
@@ -129,39 +130,43 @@ function setMaxCount(maxToasts: number): void {
  * @param {TOAST_PLACEMENT} placement Placement of the toast container.
  */
 function setPlacement(placement: TOAST_PLACEMENT): void {
-    TOAST_CONTAINER.className = "toast-container position-fixed";
-    switch (placement) {
-        case TOAST_PLACEMENT.TOP_LEFT:
-            TOAST_CONTAINER.classList.add("top-0", "start-0");
-            break;
-        case TOAST_PLACEMENT.TOP_CENTER:
-            TOAST_CONTAINER.classList.add("top-0", "start-50", "translate-middle-x");
-            break;
-        case TOAST_PLACEMENT.TOP_RIGHT:
-            TOAST_CONTAINER.classList.add("top-0", "end-0");
-            break;
-        case TOAST_PLACEMENT.MIDDLE_LEFT:
-            TOAST_CONTAINER.classList.add("top-50", "start-0", "translate-middle-y");
-            break;
-        case TOAST_PLACEMENT.MIDDLE_CENTER:
-            TOAST_CONTAINER.classList.add("top-50", "start-50", "translate-middle");
-            break;
-        case TOAST_PLACEMENT.MIDDLE_RIGHT:
-            TOAST_CONTAINER.classList.add("top-50", "end-0", "translate-middle-y");
-            break;
-        case TOAST_PLACEMENT.BOTTOM_LEFT:
-            TOAST_CONTAINER.classList.add("bottom-0", "start-0");
-            break;
-        case TOAST_PLACEMENT.BOTTOM_CENTER:
-            TOAST_CONTAINER.classList.add("bottom-0", "start-50", "translate-middle-x");
-            break;
-        case TOAST_PLACEMENT.BOTTOM_RIGHT:
-            TOAST_CONTAINER.classList.add("bottom-0", "end-0");
-            break;
-        default:
-            TOAST_CONTAINER.classList.add("top-0", "end-0");
-            break;
-    }
+  TOAST_CONTAINER.className = "toast-container position-fixed";
+  switch (placement) {
+    case TOAST_PLACEMENT.TOP_LEFT:
+      TOAST_CONTAINER.classList.add("top-0", "start-0");
+      break;
+    case TOAST_PLACEMENT.TOP_CENTER:
+      TOAST_CONTAINER.classList.add("top-0", "start-50", "translate-middle-x");
+      break;
+    case TOAST_PLACEMENT.TOP_RIGHT:
+      TOAST_CONTAINER.classList.add("top-0", "end-0");
+      break;
+    case TOAST_PLACEMENT.MIDDLE_LEFT:
+      TOAST_CONTAINER.classList.add("top-50", "start-0", "translate-middle-y");
+      break;
+    case TOAST_PLACEMENT.MIDDLE_CENTER:
+      TOAST_CONTAINER.classList.add("top-50", "start-50", "translate-middle");
+      break;
+    case TOAST_PLACEMENT.MIDDLE_RIGHT:
+      TOAST_CONTAINER.classList.add("top-50", "end-0", "translate-middle-y");
+      break;
+    case TOAST_PLACEMENT.BOTTOM_LEFT:
+      TOAST_CONTAINER.classList.add("bottom-0", "start-0");
+      break;
+    case TOAST_PLACEMENT.BOTTOM_CENTER:
+      TOAST_CONTAINER.classList.add(
+        "bottom-0",
+        "start-50",
+        "translate-middle-x"
+      );
+      break;
+    case TOAST_PLACEMENT.BOTTOM_RIGHT:
+      TOAST_CONTAINER.classList.add("bottom-0", "end-0");
+      break;
+    default:
+      TOAST_CONTAINER.classList.add("top-0", "end-0");
+      break;
+  }
 }
 
 /**
@@ -169,29 +174,29 @@ function setPlacement(placement: TOAST_PLACEMENT): void {
  * @param {TOAST_THEME} theme The toast theme.
  */
 function setTheme(theme: TOAST_THEME = null): void {
-    const header: HTMLElement = TOAST_TEMPLATE.querySelector(".toast-header");
-    const close: HTMLElement = header.querySelector(".btn-close");
-    switch (theme) {
-        case TOAST_THEME.LIGHT:
-            TOAST_TEMPLATE.style.backgroundColor = "var(--body-bg-color-light)";
-            TOAST_TEMPLATE.style.color = "var(--text-color-light)";
-            header.style.backgroundColor = "var(--header-bg-color-light)";
-            header.style.color = "var(--header-color-light)";
-            close.style.filter = "unset";
-            break;
-        case TOAST_THEME.DARK:
-            TOAST_TEMPLATE.style.backgroundColor = "var(--body-bg-color-dark)";
-            TOAST_TEMPLATE.style.color = "var(--text-color-dark)";
-            header.style.backgroundColor = "var(--header-bg-color-dark)";
-            header.style.color = "var(--header-color-dark)";
-            close.style.filter = "invert(1) grayscale(100%) brightness(200%)";
-            break;
-        default:
-            TOAST_TEMPLATE.removeAttribute("style");
-            header.removeAttribute("style");
-            close.removeAttribute("style");
-            break;
-    }
+  const header: HTMLElement = TOAST_TEMPLATE.querySelector(".toast-header");
+  const close: HTMLElement = header.querySelector(".btn-close");
+  switch (theme) {
+    case TOAST_THEME.LIGHT:
+      TOAST_TEMPLATE.style.backgroundColor = "var(--body-bg-color-light)";
+      TOAST_TEMPLATE.style.color = "var(--text-color-light)";
+      header.style.backgroundColor = "var(--header-bg-color-light)";
+      header.style.color = "var(--header-color-light)";
+      close.style.filter = "unset";
+      break;
+    case TOAST_THEME.DARK:
+      TOAST_TEMPLATE.style.backgroundColor = "var(--body-bg-color-dark)";
+      TOAST_TEMPLATE.style.color = "var(--text-color-dark)";
+      header.style.backgroundColor = "var(--header-bg-color-dark)";
+      header.style.color = "var(--header-color-dark)";
+      close.style.filter = "invert(1) grayscale(100%) brightness(200%)";
+      break;
+    default:
+      TOAST_TEMPLATE.removeAttribute("style");
+      header.removeAttribute("style");
+      close.removeAttribute("style");
+      break;
+  }
 }
 
 /**
@@ -200,7 +205,7 @@ function setTheme(theme: TOAST_THEME = null): void {
  * @param type The timer type.
  */
 function enableTimers(type: TOAST_TIMERS): void {
-    timersEnabled = type;
+  timersEnabled = type;
 }
 
 /**
@@ -209,10 +214,9 @@ function enableTimers(type: TOAST_TIMERS): void {
  * @param {boolean} enabled Controls whether queue is enabled.
  */
 function enableQueue(enabled = true): void {
-    queueEnabled = enabled;
-    // Empty the queue once it's disabled.
-    if (!enabled)
-        queue = [];
+  queueEnabled = enabled;
+  // Empty the queue once it's disabled.
+  if (!enabled) queue = [];
 }
 
 /**
@@ -222,35 +226,34 @@ function enableQueue(enabled = true): void {
  * @param {IToastOptions} toastOptions Object containing all the desired toast options.
  */
 function create(toastOptions: IToastOptions): void {
-    const toastEl: HTMLElement = (<HTMLElement>TOAST_TEMPLATE.cloneNode(true));
+  const toastEl: HTMLElement = <HTMLElement>TOAST_TEMPLATE.cloneNode(true);
 
-    const toastTitle: HTMLElement = toastEl.querySelector(".toast-title");
-    toastTitle.innerText = toastOptions.title;
+  const toastTitle: HTMLElement = toastEl.querySelector(".toast-title");
+  toastTitle.innerText = toastOptions.title;
 
-    const toastBody: HTMLElement = toastEl.querySelector(".toast-body");
-    toastBody.innerHTML = toastOptions.message;
+  const toastBody: HTMLElement = toastEl.querySelector(".toast-body");
+  toastBody.innerHTML = toastOptions.message;
 
-    setStatus(toastEl, toastOptions.status);
+  setStatus(toastEl, toastOptions.status);
 
-    // Add toast to the queue if it would exceed maxToastCount
-    if (currentToastCount >= maxToastCount) {
-        if (!queueEnabled)
-            return;
+  // Add toast to the queue if it would exceed maxToastCount
+  if (currentToastCount >= maxToastCount) {
+    if (!queueEnabled) return;
 
-        const toastToQueue: IToast = {
-            toast: toastEl,
-            timeout: toastOptions.timeout
-        }
-        queue.push(toastToQueue);
-        return;
-    }
+    const toastToQueue: IToast = {
+      toast: toastEl,
+      timeout: toastOptions.timeout,
+    };
+    queue.push(toastToQueue);
+    return;
+  }
 
-    const toastInfo: IToast = {
-        toast: toastEl,
-        timeout: toastOptions.timeout
-    }
+  const toastInfo: IToast = {
+    toast: toastEl,
+    timeout: toastOptions.timeout,
+  };
 
-    render(toastInfo);
+  render(toastInfo);
 }
 
 /**
@@ -259,29 +262,29 @@ function create(toastOptions: IToastOptions): void {
  * @param {TOAST_STATUS} status The integer value representing the toast's status.
  */
 function setStatus(toastEl: HTMLElement, status: TOAST_STATUS): void {
-    const statusIcon: HTMLElement = toastEl.querySelector(".status-icon");
+  const statusIcon: HTMLElement = toastEl.querySelector(".status-icon");
 
-    switch (status) {
-        case TOAST_STATUS.SUCCESS:
-            statusIcon.classList.add("text-success", "bi-check-circle-fill");
-            break;
-        case TOAST_STATUS.DANGER:
-            statusIcon.classList.add("text-danger", "bi-x-circle-fill");
-            toastEl.setAttribute("role", "alert");
-            toastEl.setAttribute("aria-live", "assertive");
-            break;
-        case TOAST_STATUS.WARNING:
-            statusIcon.classList.add("text-warning", "bi-exclamation-circle-fill");
-            toastEl.setAttribute("role", "alert");
-            toastEl.setAttribute("aria-live", "assertive");
-            break;
-        case TOAST_STATUS.INFO:
-            statusIcon.classList.add("text-info", "bi-info-circle-fill");
-            break;
-        default:
-            statusIcon.classList.add("d-none");
-            break;
-    }
+  switch (status) {
+    case TOAST_STATUS.SUCCESS:
+      statusIcon.classList.add("text-success", "bi-check-circle-fill");
+      break;
+    case TOAST_STATUS.DANGER:
+      statusIcon.classList.add("text-danger", "bi-x-circle-fill");
+      toastEl.setAttribute("role", "alert");
+      toastEl.setAttribute("aria-live", "assertive");
+      break;
+    case TOAST_STATUS.WARNING:
+      statusIcon.classList.add("text-warning", "bi-exclamation-circle-fill");
+      toastEl.setAttribute("role", "alert");
+      toastEl.setAttribute("aria-live", "assertive");
+      break;
+    case TOAST_STATUS.INFO:
+      statusIcon.classList.add("text-info", "bi-info-circle-fill");
+      break;
+    default:
+      statusIcon.classList.add("d-none");
+      break;
+  }
 }
 
 /**
@@ -289,30 +292,29 @@ function setStatus(toastEl: HTMLElement, status: TOAST_STATUS): void {
  * @param {IToast} toastInfo The toast object to be rendered.
  */
 function render(toastInfo: IToast): void {
-    // If the timeout is 0, the toast must be dismissed manually
-    if (toastInfo.timeout > 0) {
-        toastInfo.toast.setAttribute("data-bs-delay", toastInfo.timeout.toString());
-        toastInfo.toast.setAttribute("data-bs-autohide", "true");
+  // If the timeout is 0, the toast must be dismissed manually
+  if (toastInfo.timeout > 0) {
+    toastInfo.toast.setAttribute("data-bs-delay", toastInfo.timeout.toString());
+    toastInfo.toast.setAttribute("data-bs-autohide", "true");
+  }
+
+  renderTimer(toastInfo);
+
+  TOAST_CONTAINER.appendChild(toastInfo.toast);
+  // Initialize Bootstrap 5's toast plugin
+  const bsToast = new Toast(toastInfo.toast);
+  bsToast.show();
+  currentToastCount++;
+
+  // When the toast hides, remove it from the DOM
+  toastInfo.toast.addEventListener("hidden.bs.toast", () => {
+    TOAST_CONTAINER.removeChild(toastInfo.toast);
+    currentToastCount--;
+    if (queueEnabled && queue.length > 0 && currentToastCount < maxToastCount) {
+      const queuedToast = queue.shift();
+      render(queuedToast);
     }
-
-    renderTimer(toastInfo);
-
-    TOAST_CONTAINER.appendChild(toastInfo.toast);
-    // Initialize Bootstrap 5's toast plugin
-    // @ts-ignore
-    const bsToast = new bootstrap.Toast(toastInfo.toast);
-    bsToast.show();
-    currentToastCount++;
-
-    // When the toast hides, remove it from the DOM
-    toastInfo.toast.addEventListener('hidden.bs.toast', () => {
-        TOAST_CONTAINER.removeChild(toastInfo.toast);
-        currentToastCount--;
-        if (queueEnabled && queue.length > 0 && currentToastCount < maxToastCount) {
-            const queuedToast = queue.shift();
-            render(queuedToast);
-        }
-    });
+  });
 }
 
 /**
@@ -320,62 +322,63 @@ function render(toastInfo: IToast): void {
  * @param toastInfo The toast object to be rendered.
  */
 function renderTimer(toastInfo: IToast) {
-    const timer: HTMLElement = toastInfo.toast.querySelector(".timer");
+  const timer: HTMLElement = toastInfo.toast.querySelector(".timer");
 
-    switch (timersEnabled) {
-        case TOAST_TIMERS.ELAPSED: {
-            timer.innerText = "just now";
-            // Start a timer that updates the text of the time indicator every minute
-            // Initially set to 1 because for the first minute the indicator reads "just now"
-            let minutes = 1
-            const elapsedTimer = setInterval(() => {
-                timer.innerText = `${minutes}m ago`;
-                minutes++;
-            }, 60 * 1000);
+  switch (timersEnabled) {
+    case TOAST_TIMERS.ELAPSED: {
+      timer.innerText = "just now";
+      // Start a timer that updates the text of the time indicator every minute
+      // Initially set to 1 because for the first minute the indicator reads "just now"
+      let minutes = 1;
+      const elapsedTimer = setInterval(() => {
+        timer.innerText = `${minutes}m ago`;
+        minutes++;
+      }, 60 * 1000);
 
-            // When the toast hides, delete its timer instance
-            toastInfo.toast.addEventListener('hidden.bs.toast', () => {
-                clearInterval(elapsedTimer);
-            });
-            break;
-        }
-        case TOAST_TIMERS.COUNTDOWN: {
-            if (toastInfo.timeout > 0) {
-                // Start a timer that updates the text of the time indicator every minute
-                // Initially set to 1 because for the first minute the indicator reads "just now"
-                let seconds = toastInfo.timeout / 1000;
-                timer.innerText = `${seconds}s`;
-                const countdownTimer = setInterval(() => {
-                    timer.innerText = `${seconds - 1}s`;
-                    seconds--;
-                }, 1000);
-
-                // When the toast hides, delete its timer instance
-                toastInfo.toast.addEventListener('hidden.bs.toast', () => {
-                    clearInterval(countdownTimer);
-                });
-            }
-            break;
-        }
-        default: {
-            const toastHeader: HTMLElement = toastInfo.toast.querySelector(".toast-header");
-            toastHeader.removeChild(timer);
-            break;
-        }
+      // When the toast hides, delete its timer instance
+      toastInfo.toast.addEventListener("hidden.bs.toast", () => {
+        clearInterval(elapsedTimer);
+      });
+      break;
     }
+    case TOAST_TIMERS.COUNTDOWN: {
+      if (toastInfo.timeout > 0) {
+        // Start a timer that updates the text of the time indicator every minute
+        // Initially set to 1 because for the first minute the indicator reads "just now"
+        let seconds = toastInfo.timeout / 1000;
+        timer.innerText = `${seconds}s`;
+        const countdownTimer = setInterval(() => {
+          timer.innerText = `${seconds - 1}s`;
+          seconds--;
+        }, 1000);
+
+        // When the toast hides, delete its timer instance
+        toastInfo.toast.addEventListener("hidden.bs.toast", () => {
+          clearInterval(countdownTimer);
+        });
+      }
+      break;
+    }
+    default: {
+      const toastHeader: HTMLElement =
+        toastInfo.toast.querySelector(".toast-header");
+      toastHeader.removeChild(timer);
+      break;
+    }
+  }
 }
 
 export {
-    TOAST_STATUS,
-    TOAST_PLACEMENT,
-    TOAST_THEME,
-    TOAST_TIMERS,
-    configure,
-    setMaxCount,
-    setPlacement,
-    setStatus,
-    setTheme,
-    enableQueue,
-    enableTimers,
-    create
+  TOAST_STATUS,
+  TOAST_PLACEMENT,
+  TOAST_THEME,
+  TOAST_TIMERS,
+  configure,
+  setMaxCount,
+  setPlacement,
+  setStatus,
+  setTheme,
+  enableQueue,
+  enableTimers,
+  create,
 };
